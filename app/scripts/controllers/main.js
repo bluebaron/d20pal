4ccbd5fdbf5aae2203e35fa0c0dfcefe029ad89a
Braxton Spence <brax@braxtonspence.com>
2015-03-12 18:06:35 -0500

    Initial commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name d20palApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the d20palApp
+ */
+angular.module('d20palApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
