be09293f3582cf6978218097d86ba1d322a1de43
Braxton Spence <brax@braxtonspence.com>
2015-03-14 20:31:14 -0500

    Added classes for Chainable and ChainLink.

--- app/scripts/d20pal.js
+++ app/scripts/d20pal.js
@@ -53,25 +53,35 @@ var d20pal = (function() {
         if (Array.isArray(arg)) {
           this.tag.apply(this, arg);
         } else {
-          this.tags.push(arg);
+          if (this.tags.indexOf(arg) === -1) {
+            this.tags.push(arg);
+          }
         }
       }, this);
     };
 
     /**
-     * Checks if an object is tagged with tagString.
+     * Checks if an object is tagged with the supplied tags. Will check all
+     * arrays, including nested arrays. Can't see why that'd be necessary
+     * though.
      *
-     * @param {string} tagString - Tag to check for.
-     * @returns {bool} Whether or not the object was tagged with the supplied tag.
+     * @param {...(string|string[])} tagString - Tags to check for.
+     * @returns {bool} Whether or not the object was tagged with the supplied tags.
      */
     var isTagged = function(tagString) {
       initTags(this);
       var args = Array.prototype.slice.call(arguments);
-      args.forEach(function(arg){
-        if (!Array.isArray(arg)) { // base case
-          return (this.tags.indexOf(arg) !== -1);
-        } else { // array to be recursed
-          return arg.every(this.isTagged, this);
+      return args.every(function(arg) {
+        if (Array.isArray(arg)) {
+          // Recurse into array
+          return isTagged.apply(this, arg);
+        } else {
+          // Base case
+          if (this.tags.indexOf(arg) !== -1) {
+            return true;
+          } else {
+            return false;
+          }
         }
       }, this);
     };
@@ -80,7 +90,76 @@ var d20pal = (function() {
       tag: tag,
       isTagged: isTagged
     };
-  })();
+  })(); // end of Taggable definition
+
+  /**
+   * Creates a ChainLink for use with Chainable instances.
+   * 
+   * @constructor
+   * @param {ChainLink~modifierCallback | number} - A function accepting a value
+   * appropriate for the Chainable that performs some calculation on it
+   * and returns the new value. If a number is supplied, a function will be
+   * created that only returns that number.
+   */
+  var ChainLink = function(modifierCallback) {
+    if (typeof modifierCallback === 'number') {
+      this.modifierCallback = function(oldVal) {
+        return modifierCallback; // actually returns the number
+      };
+    } else {
+      this.modifierCallback = modifierCallback;
+    }
+  };
+
+  /**
+   * Evaluates the link's callback on the supplied value and returns the
+   * new value.
+   * 
+   * @param {*} oldVal - Prior value, handed to this link.
+   * @returns {*} Value after evaluating callback on parameter.
+   */
+  ChainLink.prototype.evaluate = function(oldVal) {
+    return this.modifierCallback(oldVal);
+  };
+
+  /**
+   * Creates a Chainable with the supplied name.
+   *
+   * @constructor
+   * @param {string} name - Name of the property that is chainable.
+   * 
+   * @classdesc Chainables are properties than can be calculated by taking
+   * some base value and passing it through multiple functions in a defined
+   * order determined by each link's priority.
+   */
+  var Chainable = function(name) {
+    this.name = name;
+    this.chainLinks = [];
+  };
+
+  Chainable.prototype.addLink = function(newLink, priority) {
+    newLink.priority = priority;
+
+    var res = this.chainLinks.every(function(curLink, i) {
+      if (priority > curLink.priority) {
+        return true;
+      } else {
+        if (priority === curLink.priority) {
+          // Priorities are the same; try again with next highest
+          return this.addLink(newLink, priority+1);
+        }
+
+        this.chainLinks.splice(i, 0, newLink);
+        return false;
+      }
+    }, this);
+
+    if (res) { // newLink is lowest priority
+      this.chainLinks.push(newLink);
+    }
+
+    return newLink;
+  };
 
   /**
    * Creates a character.
@@ -90,6 +169,8 @@ var d20pal = (function() {
    */
   var Character = function(name) {
     this.name = name;
+    this.strength = new Chainable('strength');
+    this.initstrength = new ChainLink(100);
   };
 
   /**
