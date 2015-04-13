eff28ab0da432732860d5692fb54a909dd557014
Braxton Spence <brax@braxtonspence.com>
2015-04-13 00:46:08 -0500

    Fixed ChainLinks, changed some of the main view.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,5 +11,31 @@
 angular.module('d20palApp')
   .controller('MainCtrl', function ($scope) {
     var joe = new d20pal.Character('Joe');
+    window.joe = joe;
     $scope.characters = [joe];
+    $scope.selectedCharacter = null;
+
+    // Sets character highlighting in the UI
+    var setCharacterHighlighted = function(character, hl) {
+      if (character === null) return;
+      if (hl) {
+        character.selected = true;
+      } else {
+        character.selected = false;
+      }
+    };
+
+    $scope.selectCharacter = function(characterIndex) {
+      var character = $scope.characters[characterIndex];
+      if (character === undefined || character === null) {
+        console.log('Null character.');
+        return;
+      }
+
+      console.log('Selecting character ' + character.name  +'.');
+
+      setCharacterHighlighted($scope.selectedCharacter, false);
+      $scope.selectedCharacter = character;
+      setCharacterHighlighted(character, true);
+    };
   });
--- app/scripts/d20pal.js
+++ app/scripts/d20pal.js
@@ -111,11 +111,15 @@ var d20pal = (function() {
    * @memberof module:d20pal
    */
   var ChainLink = function(modifierCallback) {
+    console.log('Instantiating ChainLink.');
     if (typeof modifierCallback !== 'function') {
+      console.log('Non-function modifierCallback.');
       this.modifierCallback = function(oldVal) {
+        console.log('ChainLink default non-function modifierCallback called; returning value ' + modifierCallback);
         return modifierCallback; // actually returns the number
       };
     } else {
+      console.log('Typical modifierCallback.');
       this.modifierCallback = modifierCallback;
     }
   };
@@ -128,6 +132,8 @@ var d20pal = (function() {
    * @returns {*} Value after evaluating callback on parameter.
    */
   ChainLink.prototype.evaluate = function(oldVal, params) {
+    console.log('ChainLink evaluate method called.');
+    console.log(oldVal, params, this.modifierCallback(oldVal, params));
     return this.modifierCallback(oldVal, params);
   };
 
@@ -157,16 +163,21 @@ var d20pal = (function() {
    * @memberof module:d20pal
    */
   var Chainable = function(name) {
+    console.log('Instantiating Chainable ' + name + '.');
     this.name = name;
     if (arguments.length > 1) {
       this.startChain = arguments[1];
+      console.log('Start chain included: ' + arguments[1].name);
     } else {
       this.startChain = null;
+      console.log('Independent chain.');
     }
+
     this.chainLinks = [];
   };
 
   Chainable.prototype.addLink = function(newLink, priority) {
+    console.log('Adding link to Chainable "' + this.name + '"');
     newLink.priority = priority;
 
     var res = this.chainLinks.every(function(curLink, i) {
@@ -216,8 +227,9 @@ var d20pal = (function() {
     this.strength = new Chainable('strength');
     var initstrength = new ChainLink(100);
     var doubler = new ChainLink(function(oldVal){return oldVal*2});
-    this.strength.addLink(initstrength);
+    this.strength.addLink(initstrength, 0);
     this.strength.addLink(doubler, 100);
+    this.stats = [this.strength];
   };
 
   /**
