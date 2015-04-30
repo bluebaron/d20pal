9a7f3d6435904fe4f013f2e57a5d3cd2d8fbe96a
Braxton Spence <brax@braxtonspence.com>
2015-04-29 21:57:10 -0500

    Began some major restructuring.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,6 +23,22 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/characters', {
+        templateUrl: 'views/characters.html',
+        controller: 'CharactersCtrl'
+      })
+      .when('/characters/new', {
+        templateUrl: 'views/character-new.html',
+        controller: 'NewCharacterCtrl'
+      })
+      .when('/characters/:character', {
+        templateUrl: 'views/character.html',
+        controller: 'CharacterCtrl'
+      })
+      .when('/characters/:character/edit', {
+        templateUrl: 'views/character-edit.html',
+        controller: 'EditCharacterCtrl'
+      })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
